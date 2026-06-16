75a927d06b4370610a5f4c76b2020c7fbd3bd000
anas-virtual <anas.ahmed2@education.nsw.gov.au>
2026-06-16 10:29:36 +1000

    Create seng.py
    In this lesson, I created the basic structure of the tkinter file. My idea is creating a language teaching website inspired by Duolingo.

---
+++ seng.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+root =Tk()
+root.title("Learn Basic Hindi!") #Title
+root.geometry("1200x800") #Length and with of application
+root.resizeable(False, False)
+
+current_frame= None #Starts of with the current frame being set as none
+
+root.mainloop()
+
