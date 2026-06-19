fc73520ae6b12171c040d911df851890b94ff94e
anas-virtual <anas.ahmed2@education.nsw.gov.au>
2026-06-20 00:20:28 +1000

    Update seng.py
    Did extensive research on Tkinter, removed my previous work due to it being invalid and not running. Now i made a simple button which when you click it will display "Namaste, {username}!".  Also restructured the structure of my Tkinter beginning structure like the title, geometry, root and etc..

--- seng.py
+++ seng.py
@@ -1,11 +1,20 @@
-from tkinter import *
-from tkinter import ttk
-root =Tk()
-root.title("Learn Basic Hindi!") #Title
-root.geometry("1200x800") #Length and with of application
-root.resizeable(False, False)
+import tkinter as tk
 
-current_frame= None #Starts of with the current frame being set as none
+def greet_user():
+    username =name_entry.get()
+    
+    welcome_label.config(text=f"Namaste, {username}!")
 
+
+root = tk.Tk()
+root.title("Learn Hindi Made Easy!")
+root.geometry("500x450") # Set the window size to 500x450 pixels
+
+name_entry = tk.Entry(root)
+name_entry.pack(pady=10)
+submit_btn = tk.Button(root, text="Submit", command=greet_user)
+submit_btn.pack(pady=5)
+
+welcome_label = tk.Label(root, text="")
+welcome_label.pack(pady=10)
 root.mainloop()
-
