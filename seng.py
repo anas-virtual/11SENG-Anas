b69dd217a8e525556ce7959c12e68ee4648e61c7
anas-virtual <anas.ahmed2@education.nsw.gov.au>
2026-06-24 00:32:00 +1000

    Aesthetic
    Made Some changes to the appearance of the Welcome page. To make it more aesthetically more pleasing.

--- seng.py
+++ seng.py
@@ -6,7 +6,7 @@ except:
     pass
 root = tk.Tk()
 root.title("Learn Hindi Made Easy!")
-root.geometry("400x600") # Set the window size to 400x500 pixels
+root.geometry("400x500") # Set the window size to 400x500 pixels
 root.resizable(False, False) # Disable resizing of the window
 
 bg_image = tk.PhotoImage(file="Background SENG (1).png")
@@ -14,6 +14,8 @@ bg_label = tk.Label(root, image=bg_image, bd=0)
 bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 #Buttons
-start_button = tk.Button(root, text="Welcome!", font=("Arial", 14, "bold"), bg="#FF9933", fg="white", padx=20, pady=5, cursor="hand2")
+start_button = tk.Button(root, text="Welcome!", font=("Arial", 11, "bold"), fg="#FF9F3E", bg="white", padx=10, pady=2, cursor="hand2")
 start_button.place(x=200, y=265, anchor="center")
+
+
 root.mainloop()
