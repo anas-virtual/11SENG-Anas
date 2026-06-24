d6c3e80865617cf111e1fc44ea2f6bf9477a5de2
anas-virtual <anas.ahmed2@education.nsw.gov.au>
2026-06-24 23:38:44 +1000

    Adding Nested Data Structure for Lesson
    After taking inspiration from peers and W3schools, I had to begin with the Nested Data Structure which is like a dictionary where the value is a list of tuples. So far i only done Basic Phrases, soon i will added colours, numbers and etc.

--- seng.py
+++ seng.py
@@ -1,21 +1,23 @@
 import tkinter as tk
+import random
 import ctypes # For DPI awarness on Windows
 try: 
     ctypes.windll.shcore.SetProcessDpiAwarness(1) # This basically just makes the app look better and removes the blurriness and low resolution
 except:
     pass
-root = tk.Tk()
-root.title("Learn Hindi Made Easy!")
-root.geometry("400x500") # Set the window size to 400x500 pixels
-root.resizable(False, False) # Disable resizing of the window
 
-bg_image = tk.PhotoImage(file="Background SENG (1).png")
-bg_label = tk.Label(root, image=bg_image, bd=0)
-bg_label.place(x=0, y=0, relwidth=1, relheight=1)
-
-#Buttons
-start_button = tk.Button(root, text="Welcome!", font=("Arial", 11, "bold"), fg="#FF9F3E", bg="white", padx=10, pady=2, cursor="hand2")
-start_button.place(x=200, y=265, anchor="center")
-
-
-root.mainloop()
+LESSONS = {
+    "Basic Phrases!": [
+        ("Hello", "Namaste (नमस्ते)"),
+        ("Thank you", "Dhanyavaad (धन्यवाद)"),
+        ("Please", "Kripya (कृपया)"),
+        ("Yes", "Haan (हाँ)"),
+        ("No", "Nahi (नहीं)"),
+        ("Sorry", "Maaf Karna (माफ़ करना)"),
+        ("Good morning", "Suprabhat (सुप्रभात)"),
+        ("Good night", "Shubh raatri (शुभ रात्रि)"),
+        ("How are you?", "Aap kaise hain? (आप कैसे हैं?)"),
+        ("My name is Anas", "Mera naam Anas hai (मेरा नाम Anas है)"),
+    ],
+    
+}
